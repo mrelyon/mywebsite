b77ffdf04ee2fc10a6767793f4c76eb142a80e45
age2ho <age2ho@gmail.com>
2016-11-23 00:32:18 +0100

    first commit

---
+++ engine.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+import web
+import urllib2
+render = web.template.render('templates/')
+urls = (
+    '/', 'index',
+    '/(.*)', 'nopage'
+)
+
+
+#app title
+apptitle = 'Elyon Labs'
+
+#app description
+appdescription = [apptitle,'A boy\'s dream came true']
+
+#login info
+loginfo = ''
+
+
+#app calling
+app = web.application(urls, globals())
+
+
+#get header   
+def get_header(name,checkblog):
+        global session
+        checkuser = session.udata
+        return render.masthead(name,checkuser,appdescription,checkblog)
+
+#get footer   
+def get_footer(name):
+        global session
+        checkuser = session.udata
+        return render.mastfooter(name,checkuser,appdescription)
+    
+
+
+
+class index:
+    def GET(self):
+        name = 'Home'
+        return render.index(name,appdescription)
+
+
+#not found        
+def notfound():
+	       return web.notfound(render.notfound(render))
+          
+
+#internal error
+def internalerror():
+	       return web.internalerror(render.internalerror(render))
+               
+app.notfound = notfound
+app.internalerror = internalerror
+
+class nopage:
+    def GET(self,name):
+        return render.notfound(name)
+    
+if __name__ == "__main__":
+    app.run()
---
+++ run app.bat
@@ -0,0 +1,5 @@
+@echo off
+path %python%;C:\Python27
+python engine.py 
+http://127.0.0.0:3000
+PAUSE
